f016abdadbc70fc021dd27141344dd548257e476
Leonardo Borges <leonardoborges.rj@gmail.com>
2019-12-06 16:27:34 +1100

    Initial Commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to teams-connector-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/teams_connector_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns teams-connector-clj.core-test
+  (:require [clojure.test :refer :all]
+            [teams-connector-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
