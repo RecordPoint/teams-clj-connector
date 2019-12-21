dd8ed36621aab1b930f555fba6ab49e9891062b5
Leonardo Borges <leonardoborges.rj@gmail.com>
2019-12-21 22:53:48 +1100

    Move token request to r365

--- src/teams_connector_clj/r365.clj
+++ src/teams_connector_clj/r365.clj
@@ -1,8 +1,21 @@
 (ns teams-connector-clj.r365
   (:require [clj-http.client        :as    http]
             [environ.core           :refer [env]]
-            [camel-snake-kebab.core :as    csk]))
+            [camel-snake-kebab.core :as    csk]
+            [cheshire.core          :as    json]))
 
+
+(def oauth-params {:grant_type    "client_credentials"
+                   :client_id     (env :client-id)
+                   :client_secret (env :client-secret)
+                   :resource      (env :audience)})
+
+
+(defn request-token []
+  (-> (http/post (env :oauth-endpoint) {:form-params oauth-params})
+      :body
+      (json/parse-string true)
+      :access_token))
 
 (defn submit-record [record access-token]
   (let [record (->> record
