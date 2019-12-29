dbe4c3e6c8e5b467ac6fddf59600c5c7cabe560a
Leonardo Borges <leonardoborges.rj@gmail.com>
2019-12-29 18:04:25 +1100

    Refactor Graph namespace to transform Graph responses into simplified hashmaps

--- src/teams_connector_clj/graph_api.clj
+++ src/teams_connector_clj/graph_api.clj
@@ -12,6 +12,14 @@
   (let [endpoint (str (env :graph-api-endpoint) path)]
     (apply format endpoint params)))
 
+(defn- graph-get [uri token]
+  (-> (http/get uri
+                {:content-type  :json
+                 :accept        :json
+                 :oauth-token   token})
+      :body
+      (json/parse-string true)))
+
 
 (defn request-token []
   (-> (http/post (env :oauth-endpoint-v2) {:form-params oauth-params})
@@ -20,17 +28,22 @@
       :access_token))
 
 (defn channels [team-id token]
-  (-> (http/get (graph-uri "/v1.0/teams/%s/channels" team-id)
-                {:content-type  :json
-                 :accept        :json
-                 :oauth-token   token})
-      :body
-      (json/parse-string true)))
+  (->> (graph-get (graph-uri "/v1.0/teams/%s/channels" team-id)
+                  token)
+       :value
+       (map (fn [channel]
+              {:id          (:id channel)
+               :name        (:displayName channel)
+               :description (:description channel)}))))
 
 (defn messages [team-id channel-id token]
-  (-> (http/get (graph-uri "/beta/teams/%s/channels/%s/messages" team-id channel-id)
-                {:content-type  :json
-                 :accept        :json
-                 :oauth-token   token})
-      :body
-      (json/parse-string true)))
+  (->> (graph-get (graph-uri "/beta/teams/%s/channels/%s/messages" team-id channel-id)
+                  token)
+       :value
+       (map (fn [message]
+              {:id           (:id message)
+               :message-type (:messageType message)
+               :from         {:id   (get-in (:from message) [:user :id])
+                              :name (get-in (:from message) [:user :displayName])}
+               :created-at   (:createdDateTime message)
+               :body         {:content-type (:body message)}}))))
