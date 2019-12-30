d5b3416ebcc2691de6c1c3e05ef8f7bf46cbc1e1
Leonardo Borges <leonardoborges.rj@gmail.com>
2019-12-30 19:41:53 +1100

    Add missing subscription ack test

--- test/teams_connector_clj/core_test.clj
+++ test/teams_connector_clj/core_test.clj
@@ -1,7 +1,9 @@
 (ns teams-connector-clj.core-test
   (:require [clojure.test :refer :all]
             [teams-connector-clj.core :refer :all]
-            [ring.mock.request :as mock]))
+            [ring.mock.request :as mock]
+            [camel-snake-kebab.core :as csk]
+            [cheshire.core :as json]))
 
 (deftest app-test
   (testing "Graph API Validation Webhook"
@@ -13,9 +15,29 @@
                 :headers {"content-type" "text/plain"}
                 :body    validation-token}))))
 
-    (testing "Fails when no validation token is given"
-      (is (= (app (mock/request :post "/api/graph/subscriptions/notifications"))
-             {:status  400
+    (testing "Fails when no validation token or payload is given"
+      (is (= {:status  400
               :headers {"content-type" "text/plain"}
-              :body    "Bad request"})))))
+              :body    "Bad request"}
+             (app (mock/request :post "/api/graph/subscriptions/notifications"))))))
 
+
+  (testing "Webhook notification processing"
+    (testing "Acknowledges notification receipt"
+      (let [payload {"value" [{"subscriptionId"                 "ba1bd098-356c-49f6-a477-36f665522458"
+                               "changeType"                     "created"
+                               "tenantId"                       "b8175554-0945-4fd2-b5c9-840f12c7ae56"
+                               "clientState"                    "optional"
+                               "subscriptionExpirationDateTime" "2019-12-30T07:16:44.69+00:00"
+                               "resource"                       "teams('t-id')/channels('c-id')/messages('m-id')"
+                               "resourceData"
+                               {"@odata.type" "#Microsoft.Graph.ChatMessage"
+                                "@odata.id" "teams('t-id')/channels('c-id')/messages('m-id')"}
+                               "encryptedContent" nil}]}
+            request  (-> (mock/request :post "/api/graph/subscriptions/notifications")
+                         (mock/json-body payload))
+            response (app request)]
+
+        (are [x y] (= x y)  
+          202        (:status response)
+          "Accepted" (:body response))))))
