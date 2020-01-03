6faa3a75bbd80d3c28174c14257706f2521e5895
Leonardo Borges <leonardoborges.rj@gmail.com>
2020-01-03 19:02:28 +1100

    Several Changes to graph-api:     - Implement channel subscription     - Implement generic Graph-aware, recursive resultset pagination function `reduce-rs`     - Refactor  `messages` and `replies`  to use `reduce-rs`

--- src/teams_connector_clj/graph_api.clj
+++ src/teams_connector_clj/graph_api.clj
@@ -1,7 +1,11 @@
 (ns teams-connector-clj.graph-api
   (:require [clj-http.client        :as    http]
             [environ.core           :refer [env]]
-            [cheshire.core          :as    json]))
+            [cheshire.core          :as    json]
+            [camel-snake-kebab.core :as    csk]
+            [taoensso.timbre        :as timbre :refer [info debug error]]
+            [teams-connector-clj.util          :refer [date-time-formatter]])
+  (:import  [java.time ZoneId LocalDateTime Instant]))
 
 (def oauth-params {:grant_type    "client_credentials"
                    :client_id     (env :client-id)
@@ -13,12 +17,18 @@
     (apply format endpoint params)))
 
 (defn- graph-get [uri token]
-  (-> (http/get uri
-                {:content-type  :json
-                 :accept        :json
-                 :oauth-token   token})
-      :body
-      (json/parse-string true)))
+  (let [r (-> (http/get uri
+                        {:content-type  :json
+                         :accept        :json
+                         :oauth-token   token})
+              :body
+              (json/parse-string (fn [k]
+                                   (let [k (if (.startsWith k "@odata")
+                                             (->  k (clojure.string/replace #"@odata\." "odata-"))
+                                             k)]
+                                     (csk/->kebab-case-keyword k)))))]
+    (info (keys  r))
+    r))
 
 
 (defn request-token []
@@ -28,22 +38,48 @@
       :access_token))
 
 (defn channels [team-id token]
-  (->> (graph-get (graph-uri "/v1.0/teams/%s/channels" team-id)
-                  token)
-       :value
-       (map (fn [channel]
-              {:id          (:id channel)
-               :name        (:displayName channel)
-               :description (:description channel)}))))
+  (graph-get (graph-uri "/v1.0/teams/%s/channels" team-id)
+             token))
+
+(defn message [team-id channel-id message-id token]
+  (graph-get (graph-uri "/beta/teams/%s/channels/%s/messages/%s" team-id channel-id message-id)
+             token))
+
+(defn reduce-rs
+  "Paginates through a Graph API resultset by recursively iterating over its `next-page` attribute.
+  If no `acc` is given, it will invoke `f` without arguments to produce a seed value.
+
+  Results are combined using `f`. Once there are no more pages, the final accumulated value, `acc` is returned"
+  ([f next-page token]
+   (let [{next-page :odata-next-link
+          payload   :value}           (graph-get next-page token)]
+     (reduce-rs f (f (f) payload) next-page token)))
+  ([f acc next-page token]
+   (if next-page
+     (let [{next-page :odata-next-link
+            payload   :value}           (graph-get next-page token)]
+       (recur f (f acc payload) next-page token))
+     acc)))
+
 
 (defn messages [team-id channel-id token]
-  (->> (graph-get (graph-uri "/beta/teams/%s/channels/%s/messages" team-id channel-id)
-                  token)
-       :value
-       (map (fn [message]
-              {:id           (:id message)
-               :message-type (:messageType message)
-               :from         {:id   (get-in (:from message) [:user :id])
-                              :name (get-in (:from message) [:user :displayName])}
-               :created-at   (:createdDateTime message)
-               :body         {:content-type (:body message)}}))))
+  (reduce-rs concat
+             (graph-uri "/beta/teams/%s/channels/%s/messages" team-id channel-id)
+             token))
+
+(defn replies [team-id channel-id message-id token]
+  (reduce-rs concat
+             (graph-uri "/beta/teams/%s/channels/%s/messages/%s/replies" team-id channel-id message-id)
+             token))
+
+(defn subscribe-to-channel [team-id channel-id token]
+  (http/post (graph-uri "/beta/subscriptions")
+             {:content-type  :json
+              :accept        :json
+              :oauth-token token
+              :form-params {:changeType         "created,updated"
+                            :notificationUrl    "https://ec9e75ef.ngrok.io/api/graph/subscriptions/notifications"
+                            :resource           (format  "/teams/%s/channels/%s/messages" team-id channel-id)
+                            :expirationDateTime (.format  (.plusMinutes (LocalDateTime/now (ZoneId/of "UTC")) 10)
+                                                          date-time-formatter)
+                            :clientState        "optional"}}))
